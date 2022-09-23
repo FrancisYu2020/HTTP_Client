61fe2ad6eed80bf0af116c939e9615594b9b36b4
Cactus <hangy6@illinois.edu>
2022-09-23 17:07:51 -0500

    fopen not work

--- client.cpp
+++ client.cpp
@@ -21,17 +21,17 @@ using namespace std;
 #define MAXDATASIZE 512 // max number of bytes we can get at once 
 
 typedef struct URL {
-	string protocol = "HTTP/1.1";
-	string hostname;
-	string port = "80";
-	string path;
+	std::string protocol = "HTTP/1.1";
+	std::string hostname;
+	std::string port = "80";
+	std::string path;
 	int invalid;
 } URL;
 
-string CleanString(const char* url_raw) {
+std::string CleanString(const char* url_raw) {
 	// wget allows space in the input url, so we first skip the spaces
 	// in the urls
-	string s = "";
+	std::string s = "";
 	while(url_raw && *url_raw) {
 		if (*url_raw == ' ') url_raw ++;
 		else {
@@ -42,8 +42,8 @@ string CleanString(const char* url_raw) {
 	return s;
 }
 
-bool IsValidProtocol(string *url_ptr) {
-    string url = *url_ptr;
+bool IsValidProtocol(std::string *url_ptr) {
+    std::string url = *url_ptr;
 	for (int i=0; i < url.length() - 3; i++) {
 		if (!url.compare(i, 3, "://")) {
 			if (i != 4 || url.compare(0, 7, "http://")) {
@@ -58,7 +58,7 @@ bool IsValidProtocol(string *url_ptr) {
 	return 1;
 }
 
-URL* ParseURL(string url) {
+URL* ParseURL(std::string url) {
 	URL* ret = new URL();
 
 	if (!IsValidProtocol(&url)) {
@@ -83,7 +83,9 @@ URL* ParseURL(string url) {
 	return ret;
 }
 
-void HandleError(FILE* fp, const char* msg) {
+void HandleError(FILE* fp, const char* msg, int socketfd, URL* info) {
+	close(socketfd);
+	if (info) delete(info);
 	fclose(fp);
 	fp = fopen("output", "wb");
 	int ret = fwrite(msg, 1, strlen(msg), fp);
@@ -91,6 +93,15 @@ void HandleError(FILE* fp, const char* msg) {
 		perror("error when write message error message");
 	}
 	fclose(fp);
+}
+
+void HandleUnexpectedError(FILE* fp, const char* msg, int socketfd, URL* info) {
+	//This handles unexpected errors that are not mentioned in the MP instruction
+	close(socketfd);
+	if (info) delete(info);
+	fclose(fp);
+	perror(msg);
+	exit(1);
 }
 
 // get sockaddr, IPv4 or IPv6:
@@ -103,9 +114,13 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+void printer(const char* msg) {std::cout << *msg << std::endl;}
+void int_printer(int x) { std::cout << x << std::endl; }
+
+
 int main(int argc, char *argv[])
 {
-	string clean_url = CleanString(argv[1]); // TODO: finish this later, this does not actually handle when the url has space in command line
+	std::string clean_url = CleanString(argv[1]); // TODO: finish this later, this does not actually handle when the url has space in command line
 	URL* info = ParseURL(clean_url);
 	if (info->invalid) {
 		ofstream output;
@@ -135,7 +150,7 @@ int main(int argc, char *argv[])
 	hints.ai_socktype = SOCK_STREAM;
 
 	if ((rv = getaddrinfo(info->hostname.c_str(), (info->port).c_str(), &hints, &servinfo)) != 0) {
-		HandleError(fp, "NOCONNECTION");
+		HandleError(fp, "NOCONNECTION", sockfd, info);
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -160,7 +175,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (p == NULL) {
-		HandleError(fp, "NOCONNECTION");
+		HandleError(fp, "NOCONNECTION", sockfd, info);
 		fprintf(stderr, "client: failed to connect\n");
 		return 2;
 	}
@@ -172,7 +187,7 @@ int main(int argc, char *argv[])
 	freeaddrinfo(servinfo); // all done with this structure
 
 	// send the GET request here
-	string tmp = "GET " + info->path + " " + info->protocol + "\r\nHost: " + info->hostname + "\r\n\r\n";
+	std::string tmp = "GET " + info->path + " " + info->protocol + "\r\nHost: " + info->hostname + "\r\n\r\n";
 	// cout << tmp << endl;
 	char *msg = const_cast<char*>(tmp.c_str());
 	int len = strlen(msg);
@@ -183,11 +198,49 @@ int main(int argc, char *argv[])
 	}
 
         // cout << numbytes << tmp.length() << endl;
-	int http_response = 1;
+	// int http_response = 1;
 	int first_line = 1;
 	int total_received = 0;
-	while (1)
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		fclose(fp);
+	    perror("recv");
+	    exit(1);
+	}
+	buf[numbytes] = '\0';
+	if (strstr(buf, "404")) HandleError(fp, "FILENOTFOUND", sockfd, info);
+	char* length;
+	if ((length = strstr(buf, "Content-Length: "))) length = length + strlen("Content-Length: ");
+	char* length_end = strstr(length, "\n");
+	if (!length_end) {
+		fclose(fp);
+		perror("Failed to get the content length!");
+		exit(1);
+	}
+	char t = *length_end;
+	// printer(length);
+	*length_end = '\0';
+	// printer(length);
+	int expected_length = atoi(length);
+	*length_end = t;
+
+	if (char *body = strstr(buf, "\r\n\r\n")) {
+		int_printer(strlen(buf));
+		int_printer(strlen(body));
+		int body_pos;
+		char *tmp1 = buf;
+		while (tmp1 != (body + 4)) {
+			body_pos ++;
+			tmp1 ++;
+		}
+		int_printer(body_pos);
+		fwrite(buf, 1, numbytes - body_pos + 1, fp);
+		total_received += numbytes - body_pos + 1;
+	} else HandleUnexpectedError(fp, "Failed to get the end of the header!", sockfd, info);
+
+	while (total_received < expected_length)
 	{
+		// std::cout << total_received << "    " << expected_length<< endl;
 		if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
 			// output.close();
 			fclose(fp);
@@ -198,39 +251,8 @@ int main(int argc, char *argv[])
 
 		buf[numbytes] = '\0';
 		total_received += numbytes;
-		if (!strlen(buf)) break;
+		// if (!strlen(buf)) break;
 
-		if (http_response) {
-			// this code snippet handles when the buffer is still reading the header
-			// cout << buf << endl;
-			if (first_line && strstr(buf, "404")) {
-				//only when we find 404 code in the first line will we say filenotfound
-				fclose(fp);
-				fp = fopen("output", "wb");
-				fwrite("FILENOTFOUND", 1, strlen("FILENOTFOUND"), fp);
-				// output.close();
-				// output.open("output");
-				// output << "FILENOTFOUND";
-				break;
-			}
-			first_line = 0;
-			if (char *body = strstr(buf, "\r\n\r\n")) {
-				int body_pos;
-				char *tmp = buf;
-				while (tmp != (body + 4)) {
-					body_pos ++;
-					tmp ++;
-				}
-				http_response = 0;
-				fwrite(buf, 1, numbytes - body_pos, fp);
-				// output.write(body + 4, strlen(body + 4));
-			}
-			continue;
-		}
-
-		// printf("client: received '%s'\n",buf);
-		// write to the file named "output"
-		// output.write(buf, numbytes);
 		fwrite(buf, 1, numbytes, fp);
 		// break;
 	}
@@ -240,7 +262,8 @@ int main(int argc, char *argv[])
 	
 	delete(info);
 
-	output.close();
+	// output.close();
+	fclose(fp);
 
 	return 0;
 }
